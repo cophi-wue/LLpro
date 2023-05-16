4cae990093e950c3fe777825f50d7d3d7834869d
Anton Ehrmanntraut <anton.ehrmanntraut@uni-wuerzburg.de>
2023-05-16 16:08:22 +0200

    Docker: change download url of RNNTagger

--- prepare.sh
+++ prepare.sh
@@ -6,8 +6,11 @@ cd resources
 $WGET 'https://corpora.linguistik.uni-erlangen.de/someweta/german_newspaper_2020-05-28.model'
 $WGET 'https://pub.cl.uzh.ch/users/sennrich/zmorge/transducers/zmorge-20150315-smor_newlemma.ca.zip'
 unzip -u zmorge-20150315-smor_newlemma.ca.zip
-$WGET 'https://www.cis.uni-muenchen.de/~schmid/tools/RNNTagger/data/RNNTagger-1.3.zip'
-unzip -uo RNNTagger-1.3.zip
+#$WGET 'https://www.cis.uni-muenchen.de/~schmid/tools/RNNTagger/data/RNNTagger-1.3.zip'
+#unzip -uo RNNTagger-1.3.zip
+# NOTE: the original RNNTagger v1.3 seems to be unavailaible. As replacement, use v1.4.2
+$WGET 'https://www.cis.uni-muenchen.de/~schmid/tools/RNNTagger/data/RNNTagger-1.4.2.zip'
+unzip -uo RNNTagger-1.4.2.zip
 find ./RNNTagger/lib/ -type f ! -name '*german*' -delete # remove unncessesary models
 test -f "rwtagger_models.zip" || $WGET 'http://www.redewiedergabe.de/models/models.zip' -O rwtagger_models.zip
 unzip -u rwtagger_models.zip -d rwtagger_models
